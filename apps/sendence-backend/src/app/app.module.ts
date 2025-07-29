9a9dc218854ca442f027412b06914cf011918d04
Dimitrios Katsaros <dimitris.s.katsaros@gmail.com>
2025-07-29 13:18:44 +0200

    {feature} update app.module, make use of .env, update packages

--- apps/sendence-backend/src/app/app.module.ts
+++ apps/sendence-backend/src/app/app.module.ts
@@ -1,10 +1,29 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MessagesModule } from './messages/messages-module';
 
 @Module({
-  imports: [MessagesModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.get('DB_HOST'),
+        port: parseInt(config.get('DB_PORT'), 10),
+        username: config.get('DB_USER'),
+        password: config.get('DB_USER_PASSWORD'),
+        database: config.get('DB_NAME'),
+        entities: [__dirname + '/messages/entities/*.entity{.ts,.js}'],
+        synchronize: true, //TODO: set to false in production
+      }),
+      inject: [ConfigService],
+    }),
+    MessagesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
