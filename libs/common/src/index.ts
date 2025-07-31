fdd7d93e39f869a49b09804659c117fa1ffca73e
Dimitrios Katsaros <dimitris.s.katsaros@gmail.com>
2025-07-31 13:27:14 +0200

    {feature} make use of constants instead of string for event and actions

--- libs/common/src/index.ts
+++ libs/common/src/index.ts
@@ -1,2 +1,3 @@
 export * from './lib/common';
-export { Message, CreateMessage } from './lib/messages/messages.interfaces';
+export { Message, CreateMessage } from './lib/models/messages.models';
+export { RESULT, ACTION } from './lib/constants/chat.constants';
---
+++ libs/common/src/lib/constants/chat.constants.ts
@@ -0,0 +1,10 @@
+export const RESULT = {
+  MESSAGE_SENT: 'messageSent',
+  MESSAGE_ERROR: 'messageError',
+  USER_CONNECTED: 'userConnected',
+  USER_DISCONNECTED: 'userDisconnected',
+};
+
+export const ACTION = {
+  SEND_MESSAGE: 'sendMessage',
+};
